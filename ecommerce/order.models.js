9d6b219e64d2f3485ea94df870ceec58578e285a
cipher80 <117077565+cipher80@users.noreply.github.com>
2023-12-25 13:30:52 +0530

    Initial commit

---
+++ ecommerce/order.models.js
@@ -0,0 +1,40 @@
+import mongoose, { mongo } from 'mongoose';
+
+const orderItemsSchema = mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      typr: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemsSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Pending', 'Cancelled', 'Delivered'],
+      default: 'Pending',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
---
+++ ecommerce/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
